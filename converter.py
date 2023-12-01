754174582c22c1bfe1e0b24c74955f1920e13aa9
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-12-01 11:57:04 +0800

    Add GIFT format for Multiple Choice

--- converter.py
+++ converter.py
@@ -1,5 +1,8 @@
 from pdf2image import convert_from_path
 import pytesseract
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
 
 class Converter:
     @staticmethod
@@ -13,3 +16,89 @@ class Converter:
             text += pytesseract.image_to_string(img, lang='eng')
 
         return text
+
+    @staticmethod
+    def json_to_pdf(assessment):
+        # Create a PDF document
+        pdf_canvas = canvas.Canvas(r"Project Files\assessment.pdf", pagesize=letter)
+
+        # Extract information from the JSON
+        assessment_type = assessment.get("type", "")
+        questions = assessment.get("questions", [])
+        Converter.generate_answer_key(questions)
+
+        # Add content to the PDF
+        pdf_canvas.setFont("Helvetica", 12)
+        pdf_canvas.drawString(100, 800, f"Assessment Type: {assessment_type}")
+
+        y_position = 780
+        for index, question in enumerate(questions, start=1):
+            y_position -= 20  # Adjust the vertical position for each question
+
+            question_text = question.get("question", "")
+            options = question.get("options", [])
+            answer_index = question.get("answer", 0)
+
+            # Add question to the PDF
+            pdf_canvas.drawString(100, y_position, f"{index}. {question_text}")
+
+            # Add options to the PDF
+            for option_index, option in enumerate(options, start=1):
+                y_position -= 15  # Adjust the vertical position for each option
+                pdf_canvas.drawString(120, y_position, f"{chr(96 + option_index)}. {option}")
+
+            y_position -= 20  # Adjust the vertical position for the next question
+
+        # Save the PDF
+        pdf_canvas.save()
+    
+    @staticmethod
+    def generate_answer_key(questions):
+        # Create a PDF document for the answer key
+        pdf_canvas = canvas.Canvas(r"Project Files\answer_key.pdf", pagesize=letter)
+
+        questions 
+
+        # Add content to the PDF
+        pdf_canvas.setFont("Helvetica", 12)
+
+        y_position = 780
+        for index, question in enumerate(questions, start=1):
+            y_position -= 20  # Adjust the vertical position for each question
+
+            correct_answer = f"Question {index}: {chr(96 + question['answer'])}"
+            pdf_canvas.drawString(120, y_position, correct_answer)
+
+            y_position -= 30  # Adjust the vertical position for the next question
+
+        # Save the PDF
+        pdf_canvas.save()
+
+    def json_to_gift(assessment):
+        gift_output = ""
+
+        # Extract information from the JSON
+        assessment_type = assessment.get("type", "")
+        questions = assessment.get("questions", [])
+
+        # Add a title for the assessment type
+        gift_output += f"::{assessment_type}::\n\n"
+
+        # Process each question
+        for index, question in enumerate(questions, start=1):
+            question_text = question.get("question", "")
+            options = question.get("options", [])
+            answer_index = question.get("answer", 0)
+
+            # Add question title and text
+            gift_output += f"::{index} {question_text}::\n"
+
+            # Add options
+            for option_index, option in enumerate(options, start=1):
+                gift_output += f"{'=' if option_index == answer_index else '~'}{option} "
+
+            gift_output += "\n\n"
+
+            
+            with open(r"Project Files\assessment_gift.txt", "w") as file:
+                file.write(gift_output)
--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 from assessment_generator import AI
 from converter import Converter
+import json
 
 print("Assessment Generator CLI")
 
-# user_input["Lesson"] = input("Enter Lesson: ")
-print("1. Text")
-print("2. PDF")
-print("3. Txt File")
-lesson_upload = input("Enter the number of the way you want to input the lesson: ")
+# # user_input["Lesson"] = input("Enter Lesson: ")
+# print("1. Text")
+# print("2. PDF")
+# print("3. Txt File")
+# lesson_upload = input("Enter the number of the way you want to input the lesson: ")
 
-if lesson_upload == "1":
-    lesson_in_text = input("Enter Lesson: ")
-elif lesson_upload == "2":
-    lesson_pdf = input("Enter Lesson PDF Absolute File Path: ")
-    lesson_in_text = Converter.pdf_to_text(lesson_pdf)
-elif lesson_upload == "3":
-    lesson_txt = input("Enter Lesson TXT Absolute File Path: ")
-    with open(lesson_txt, 'r') as f:
-        lesson_in_text = f.read()
-else:
-    exit()
+# if lesson_upload == "1":
+#     lesson_in_text = input("Enter Lesson: ")
+# elif lesson_upload == "2":
+#     lesson_pdf = input("Enter Lesson PDF Absolute File Path: ")
+#     lesson_in_text = Converter.pdf_to_text(lesson_pdf)
+# elif lesson_upload == "3":
+#     lesson_txt = input("Enter Lesson TXT Absolute File Path: ")
+#     with open(lesson_txt, 'r') as f:
+#         lesson_in_text = f.read()
+# else:
+#     exit()
 
-assessment_type = input("Enter Type of Assessment: ")
-question_number = int(input("Enter Number of Questions: "))
+# assessment_type = input("Enter Type of Assessment: ")
+# question_number = int(input("Enter Number of Questions: "))
 
-number_of_outcomes = int(input("Enter Number of Learning Outcomes: "))
-learning_outcomes = ""
-for i in range(number_of_outcomes):
-    outcome = input(f"Enter Learning Outcome #{i+1}: ")
-    learning_outcomes += f"{i+1}. {outcome}\n"
+# number_of_outcomes = int(input("Enter Number of Learning Outcomes: "))
+# learning_outcomes = ""
+# for i in range(number_of_outcomes):
+#     outcome = input(f"Enter Learning Outcome #{i+1}: ")
+#     learning_outcomes += f"{i+1}. {outcome}\n"
 
 
-ai = AI()
-assessment_json = ai.get_assessment(lesson_in_text, assessment_type, question_number, learning_outcomes)
+# ai = AI()
 
-print(f"Assessment Type: {assessment_json['type']}")
+# # Generate the Lesson
+# assessment_json = ai.get_assessment(lesson_in_text, assessment_type, question_number, learning_outcomes)
+
+# print(f"Assessment Type: {assessment_json['type']}")
+
+# For testing purposes, we will use the sample assessment
+with open('assessment.json', 'r') as f:
+    # Load the JSON data
+    assessment_json = json.load(f)
+
+# Save the Lesson to a PDF file
+Converter.json_to_pdf(assessment_json)
+
+# Save the Lesson to a GIFT file
+Converter.json_to_gift(assessment_json)
